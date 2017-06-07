e210a0077b613739712beebc112e30e5730f97ad
seaneil01 <seaneil01@users.noreply.github.com>
2017-06-07 16:55:57 -0500

    auto tag when create in place

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -12,7 +12,7 @@ class TagsController < ApplicationController
 
   def show
     @tag = Tag.find(params[:id])
-
+    @tagged = Tagged.new
     render("tags/show.html.erb")
   end
 
@@ -39,6 +39,27 @@ class TagsController < ApplicationController
 
     if save_status == true
       redirect_to("/tags", :notice => "Tag created successfully.")
+    else
+      render("tags/new.html.erb")
+    end
+  end
+
+  def create_tag_in_place
+    @place = params[:place_id]
+
+    @tag = Tag.new
+    @tag.name = params[:name]
+    @tag.user_id = params[:user_id]
+    save_status = @tag.save
+
+    @tagged = Tagged.new
+    @tags = Tag.all
+    @tagged.place_id = params[:place_id]
+    @tagged.tag_id = @tag.id
+    save_status = @tagged.save
+
+    if save_status == true
+      redirect_to("/places/#{@place}", :notice => "Tag created successfully.")
     else
       render("tags/new.html.erb")
     end
@@ -71,9 +92,9 @@ class TagsController < ApplicationController
     @tag.destroy
 
     if URI(request.referer).path == "/tags/#{@tag.id}"
-      redirect_to("/", :notice => "Tag deleted.")
+      redirect_to("/tags", :notice => "Tag deleted.")
     else
-      redirect_to("/", :notice => "Tag deleted.")
+      redirect_to("/tags", :notice => "Tag deleted.")
     end
   end
 end
